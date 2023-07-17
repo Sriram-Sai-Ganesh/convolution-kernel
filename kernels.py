8a9fb81dcdce222148a1c953d82221fd5f246ea6
ram <35802333+Sriram-Sai-Ganesh@users.noreply.github.com>
2023-07-16 23:47:46 -0400

    removed @param dimension from get_kernel

--- kernels.py
+++ kernels.py
@@ -2,15 +2,15 @@ import math
 import numpy as np
 
 # returns 3x3 matrix of specified kernel
-def get_kernel(kernel_name, side_length):
-	dimensions = (side_length, side_length)
+def get_kernel(kernel_name):
+	dimensions = (3,3)
 	match kernel_name:
 		case "identity":
 			return identity(dimensions)
 		case "blur":
-			return blur(dimensions)
+			return average_blur(dimensions)
 		case "gaussian blur":
-			return gaussian_blur(side_length)		
+			return gaussian_blur(9)		
 		case "none":
 			return np.zeros(dimensions)
 		case other:
@@ -21,13 +21,13 @@ def identity(dimensions):
 	result[math.floor(dimensions[0]/2)][math.floor(dimensions[1]/2)] = 1
 	return result
 
-def blur(dimensions):
+def average_blur(dimensions):
 	return np.ones(dimensions)/(dimensions[0]*dimensions[1])
 
 # code modified from @clemisch 
 # create a gaussian kernel
 # @param sigma controls blur intensity
-def gaussian_blur(side=3, sigma=100.0):
+def gaussian_blur(side=9, sigma=100.0):
     ax = np.linspace(-(side - 1) / 2., (side - 1) / 2., side)
     gauss = np.exp(-0.5 * np.square(ax) / np.square(sigma))
     kernel = np.outer(gauss, gauss)
