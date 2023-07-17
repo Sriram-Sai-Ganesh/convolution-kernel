98665a3563361ce38bbfa0008ca6eed916729f04
ram <35802333+Sriram-Sai-Ganesh@users.noreply.github.com>
2023-07-16 23:48:10 -0400

    added image convolution

--- process.py
+++ process.py
@@ -3,14 +3,11 @@ import time
 
 from kernels import get_kernel
 
-# process frames from 
-def process_video(source_name = "", kernel_name = "none", side_length = 3, output_file = False):
+# process image frames from webcam by default, video files if specified.
+def process_video(source_name = "", kernel = get_kernel("identity")):
 	
 	source = cv2.VideoCapture(0) if source_name=="" else cv2.VideoCapture(source_name)
-	kernel = get_kernel(kernel_name, side_length)
-	
-	print("\nUsing kernel: ")
-	print(kernel,"\n\nConvolution time (per frame):")
+	print("\n\nConvolution time (per frame):")
 
 	# run until video cap is closed, or 'q' is pressed.
 	while source.isOpened() and (cv2.waitKey(25) & 0xFF != ord('q')):
@@ -25,17 +22,31 @@ def process_video(source_name = "", kernel_name = "none", side_length = 3, outpu
 		# wait 1ms between frames (not a bottleneck)
 		cv2.waitKey(1)
 
-def process_image(source=""):
-	if source=="":
+def process_image(source_name = "", kernel = get_kernel("identity")):
+	if source_name == "":
 		raise Exception("process.py: No image source specified.")
+	image = cv2.imread(source_name)
+	filtered_image = process_frame(image, kernel)
+	cv2.imshow(kernel_name, filtered_image)
+	cv2.waitKey()
+	outfile_name = "output/"+kernel_name+" "+source_name.split("/")[-1]
+	cv2.imwrite(outfile_name, filtered_image)
+	cv2.destroyAllWindows()
 
 def process_frame(image, kernel):
 	# Get height and width of the frame.
 	h, w = image.shape[:2]
 	start_time = time.time()
 	result = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
+	print("Frame %i x %ip :  "%(h,w), end="")
 	print("%5.3f"%((time.time() - start_time)*1000), end="")
 	print("ms")
 	return result
 
-process_video(kernel_name = "gaussian blur", side_length=3)
+
+kernel_name = "gaussian blur"
+kernel = get_kernel(kernel_name)
+print("\nKernel dimensions: ",len(kernel),"x",len(kernel[0]))
+print("Using kernel: ", kernel)
+# process_video(kernel=kernel)
+process_image(source_name = "input/barn.jpg", kernel = kernel)
